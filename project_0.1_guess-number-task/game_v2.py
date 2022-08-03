f39570c0f571092f98c63c19528ae9e1e1d457a6
AndreiDS63 <dolzhikov.as@gmail.com>
2022-08-03 14:00:17 +0400

    added dinamic border conditions

---
+++ project_0.1_guess-number-task/game_v2.py
@@ -0,0 +1,60 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    lower_border = 1
+    upper_border = 101 # задаём граничные условия
+
+    while True:
+        count += 1
+
+        predict_number = np.random.randint(lower_border, upper_border)  # предполагаемое число
+        
+        if predict_number > number:
+            upper_border = predict_number # изменение граничных условий
+        elif predict_number < number:
+            lower_border = predict_number
+        
+        elif number == predict_number:
+            break  # выход из цикла если угадали
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
