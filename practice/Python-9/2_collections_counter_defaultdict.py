5a0ef6e02451e48fc60287ebab3f65c00d5ccb25
AndreiDS63 <dolzhikov_as@mail.ru>
2022-08-03 17:28:31 +0400

    practice with counter added

---
+++ practice/Python-9/2_collections_counter_defaultdict.py
@@ -0,0 +1,53 @@
+from collections import Counter
+
+cars = ['red', 'blue', 'black', 
+        'black', 'black', 'red', 
+        'blue', 'red', 'white'
+        ]
+
+# c = Counter()
+# for car in cars:
+#     c[car] += 1
+ 
+# print(c)
+
+c = Counter(cars)
+print(c)
+print(c['black'])
+print(c['purple'])
+
+# сумма всех значений в объекте
+print(sum(c.values()))
+print(c.values())
+
+
+cars_moscow = ['black', 'black', 'white', 
+               'black', 'black', 'white', 
+               'yellow', 'yellow', 'yellow'
+               ]
+
+cars_spb = ['red', 'black', 'black', 
+            'white', 'white', 'yellow', 
+            'yellow', 'red', 'white'
+            ]
+
+counter_moscow = Counter(cars_moscow)
+counter_spb = Counter(cars_spb)
+
+print(f'counter_moscow: {counter_moscow}')
+print(f'counter_spb: {counter_spb}')
+
+# summ of counters
+print(counter_moscow + counter_spb)
+
+# difference of counters
+counter_moscow.subtract(counter_spb)
+print(counter_moscow)
+
+# Пересоздаём счётчики, потому что объект counter_moscow поменял свои значения
+# после функции subtract.
+counter_moscow = Counter(cars_moscow)
+counter_spb = Counter(cars_spb)
+ 
+print(counter_moscow - counter_spb)
+# Counter({'black': 2, 'yellow': 1})
